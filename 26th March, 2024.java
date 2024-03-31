516944fe8446b6bada48eb012b7a1dacf6b71f32
Shourya Shri <112325367+Shourya-Shri@users.noreply.github.com>
2024-03-31 20:48:11 +0530

    Create 26th March, 2024.java

---
+++ 26th March, 2024.java
@@ -0,0 +1,36 @@
+/*
+Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+*/
+
+class Solution {
+    // Function to swap elements in the array
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        // Place each positive integer i at index i-1 if possible
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        // Find the first missing positive integer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        // If all positive integers from 1 to n are present, return n + 1
+        return n + 1;
+    }
+}
